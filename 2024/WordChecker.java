d75028af88aa93c686b89aeb220eacd0cc4b3206
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-01 22:02:43 +0700

    Update WordChecker.java

--- 2024/WordChecker.java
+++ 2024/WordChecker.java
@@ -1,1 +1,14 @@
-
+public class WordChecker {
+    private ArrayList<String> wordList;
+    
+    public boolean isWordChain() {
+        for (int i = 1; i < wordList.size(); i++) {
+            String current = wordList.get(i);
+            String previous = wordList.get(i - 1);
+            if (current.indexOf(previous) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
