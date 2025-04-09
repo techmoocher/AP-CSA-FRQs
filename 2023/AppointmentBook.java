6fe2cd1be8a385447652b87c84aea0f33a011f9c
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-09 12:29:14 +0700

    Update AppointmentBook.java

--- 2023/AppointmentBook.java
+++ 2023/AppointmentBook.java
@@ -1,1 +1,12 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 9, 2025
+*/
 
+/* Question 1 */
+
+public class AppointmentBook {
+	private boolean isMinuteFree(int period, int minute) {
+		
+	}
+}
