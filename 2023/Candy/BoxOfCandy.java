3f6e743efb38b26be62c4d2e8b0dae1f7b687adc
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-10 21:36:43 +0700

    Update BoxOfCandy.java

--- 2023/Candy/BoxOfCandy.java
+++ 2023/Candy/BoxOfCandy.java
@@ -9,7 +9,17 @@ public class BoxOfCandy {
     private Candy[][] box;
     
     public boolean moveCandyToFirstRow(int col) {
-        
+        if (box[0][col] != null) {
+            return true
+        }
+        for (int i = 1; i < box.length; i++) {
+            if (box[i][col] != null) {
+                box[0][col] = box[i][col];
+                box[i][col] = null;
+                return true;
+            }
+        }
+        return false;
     }
     
     public Candy removeNextbyFlavor(String flavor) {
