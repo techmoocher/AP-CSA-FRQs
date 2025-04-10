cb2625b80861ff92e4aaa07599da516b2763a81c
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-10 21:07:25 +0700

    Update Sign.java

--- 2023/Sign.java
+++ 2023/Sign.java
@@ -1,16 +1,43 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 10, 2025
+*/
+
+/* Question 2 */
+
 public class Sign {
 	String message;
     int width;
     
-    public Class(String msg, int msg_width) {
+    public Sign(String msg, int msg_width) {
         message = msg;
         width = msg_width;
     }
     
     public int numberOfLines() {
+        if (message.length() == 0) {
+            return 0;
+        }
         if (message.length() % width == 0) {
             return message.length() / width;
         }
         return message.length() / width + 1;
+    }
+    
+    public String getLines() {
+        if (message.equals("")) {
+            return null;
+        }
+        
+        String result = "";
+        int current = width;
+        for (int i = 0; i < message.length(); i += width) {
+            int end = (i + width > message.length()) ? message.length() : i + width;
+            result += message.substring(i, end);
+            if (end < message.length()) {
+                result += ";";
+            }
+        }
+        return result;
     }
 }
