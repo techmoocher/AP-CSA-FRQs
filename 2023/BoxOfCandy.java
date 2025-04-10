229c3afb9f8e22dea48d280ae269fc76ba7d772a
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-10 21:26:51 +0700

    Create BoxOfCandy.java

---
+++ 2023/BoxOfCandy.java
@@ -0,0 +1,18 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 10, 2025
+*/
+
+/* Question 4 */
+
+public class BoxOfCandy {
+    private Candy[][] box;
+    
+    public boolean moveCandyToFirstRow(int col) {
+        
+    }
+    
+    public Candy removeNextbyFlavor(String flavor) {
+        
+    }
+}
