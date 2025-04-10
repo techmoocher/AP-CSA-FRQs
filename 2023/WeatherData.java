c2b0dd593457c692aff9dc15592c467005b188d6
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-10 21:23:37 +0700

    Create WeatherData.java

---
+++ 2023/WeatherData.java
@@ -0,0 +1,27 @@
+
+
+import java.util.ArrayList;
+
+public class WeatherData {
+    private ArrayList<Double> temperatures;
+    
+    public void cleanData(double lower, double upper) {
+        for (int i = temperatures.size() - 1; i >= 0; i--) {
+            if (temperatures.get(i) < lower || temperatures.get(i) > upper) {
+                temperatures.remove(i);
+            }
+        }
+    }
+    
+    public int longestHeatWave(double threshold) {
+        int maximum = 0;
+        int current = 0;
+        for (double value : temperatures) {
+            current = (value > threshold) ? current + 1 : 0;
+            if (current >= 2 && current > maximum) {
+                maximum = current;
+            }
+        }
+        return maximum;
+    }
+}
