a4c6fee835abaa58af216a858096b3c956eee5e9
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-10 22:09:24 +0700

    Update AppointmentBook.java

--- 2023/AppointmentBook.java
+++ 2023/AppointmentBook.java
@@ -8,19 +8,23 @@
 public class AppointmentBook {
     
     private boolean isMinuteFree(int period, int minute) {
+        /* implementation not shown */
+        
         /*
          *  Returns true if minute in period is avialable for an appointment
          *  and returns false otherwise
-		 *
+         *
          *  Preconditions: 1 <= period <= 8; 0 <= minute <= 59;
          */
     }
     private void reserveBlock(int period, int startMinute, int duration) {
+        /* implementation not shown */
+        
         /*
          *  Marks the block of minutes that starts at startMinute in period
          *  and duration minutes long as reserved for an appointment
-		 *
-         *	Preconditions: 1 <= period <= 8; 0 <= startMinute <= 59; 1 <= duration <= 60;
+         *
+         *  Preconditions: 1 <= period <= 8; 0 <= startMinute <= 59; 1 <= duration <= 60;
          */
     }
     
@@ -30,7 +34,7 @@ public class AppointmentBook {
             if (isMinuteFree(period, i)) {
                 count++;
                 if (count == duration) {
-                    return count - (duration - 1);
+                    return i - (duration - 1);
                 }
             }
             else {
@@ -38,5 +42,16 @@ public class AppointmentBook {
             }
         }
         return -1;
+    }
+    
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+        for (int i = startPeriod; i <= endPeriod; i++) {
+            int minute = findFreeBlock(i, duration);
+            if (minute != -1) {
+                reserveBlock(i, minute, duration);
+                return true;
+            }
+        }
+        return false;
     }
 }
