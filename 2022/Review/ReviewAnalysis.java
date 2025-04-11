fd8428b60ebc5d3c2c88a8facded04775b6f6ac3
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-11 10:53:23 +0700

    Update ReviewAnalysis.java

--- 2022/Review/ReviewAnalysis.java
+++ 2022/Review/ReviewAnalysis.java
@@ -6,5 +6,31 @@
 /* Question 3 */
 
 public class ReviewAnalysis {
-
+    private Review[] allReviews;
+    
+    public ReviewAnalysis() {
+        // implementation not shown
+    }
+    
+    public double getAverageRating() {
+        double result = 0.0;
+        for (Review review : allReviews) {
+            result += review.getRating();
+        }
+        return result / allReviews.length;
+    }
+    
+    public ArrayList<String> collectComments() {
+        ArrayList<String> results = new ArrayList<String>();
+        for (int i = 0; i < allReviews.length; i++) {
+            String current = allReviews[i].getComment();
+            if (current.indexOf("!") != -1) {
+                if (current.charAt(current.length() - 1) != '!' && current.charAt(current.length() - 1) != '.')) {
+                    current += ".";
+                }
+                results.add(i + "-" + current);
+            }
+        }
+        return results;
+    }
 }
