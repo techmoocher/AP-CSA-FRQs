ef64928d6044879f00c7b19696ae9fc50453e1c1
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-11 10:06:59 +0700

    Create Book.java

---
+++ 2022/Book/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String title;
+    private double price;
+    public Book (String bookTitle, double bookPrice) {
+        title = bookTitle;
+        price = bookPrice;
+        // implementation not shown
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getBookInfo() {
+        return title + "-" + price;
+    }
+}
