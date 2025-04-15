f58669ce416c91711fa1e8464b1c031d55504f4b
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-15 22:24:58 +0700

    Delete 2023/Candy directory

--- 2023/Candy/Candy.java
+++
@@ -1,7 +0,0 @@
-public class Candy {
-	// There may be instance variables, constructors, and methods that are not shown
-	public String getFlavor() {
-		/* Returns a String representing the flavor of this piece of candy */
-		/* implementation not shown */
-	}
-}
