044bd7ab6cf3628f3f65933f73849d4628312282
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-15 22:22:28 +0700

    Delete 2022/Game directory

--- 2022/Game/Level.java
+++
@@ -1,9 +0,0 @@
-public class Level {
-    public boolean goalReached() {
-        /* Returns true if the player reach the goal for this level and false otherwise. */
-    }
-    
-    public int getPoints() {
-        /* Returns the number of points (positive int) recorded in this level */
-    }
-}
