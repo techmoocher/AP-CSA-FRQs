4c867c480ee3379038ec81dc7c5d74582cb5ae31
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-15 22:25:38 +0700

    Create GridPath.java

---
+++ 2024/GridPath.java
@@ -0,0 +1,30 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 8, 2025
+*/
+
+/* Question 4 */
+
+/* Implementation for Location class is not shown */
+public class GridPath {
+    private int[][] grid;
+    
+    public Location getNextLoc(int row, int col) {
+        if (row == grid.length - 1) {
+            return new Location(row, col + 1);
+        }
+        if (col = grid[0].length - 1) {
+            return new Location(row + 1, col);
+        }
+        if (grid[row][col + 1] < grid[row + 1][col]) {
+            return new Location(row, col + 1);
+        }
+        else {
+            return new Location(row + 1, col);
+        }
+    }
+    
+    public int sumPath(int row, int col) {
+        
+    }
+}
