8985a07c3ab8bc7f5870d55914c95e7f73ce9754
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-15 22:23:50 +0700

    Create ReviewAnalysis.java

---
+++ 2022/ReviewAnalysis.java
@@ -0,0 +1,37 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 11, 2025
+*/
+
+/* Question 3 */
+
+/* Implementation for Review class is not shown */
+public class ReviewAnalysis {
+    private Review[] allReviews;
+    
+    public ReviewAnalysis() {
+        // implementation not shown
+    }
+    
+    public double getAverageRating() {
+        double result = 0.0;
+        for (Review review : allReviews) {
+            result += review.getRating();
+        }
+        return result / allReviews.length;
+    }
+    
+    public ArrayList<String> collectComments() {
+        ArrayList<String> results = new ArrayList<String>();
+        for (int i = 0; i < allReviews.length; i++) {
+            String current = allReviews[i].getComment();
+            if (current.indexOf("!") != -1) {
+                if (current.charAt(current.length() - 1) != '!' && current.charAt(current.length() - 1) != '.')) {
+                    current += ".";
+                }
+                results.add(i + "-" + current);
+            }
+        }
+        return results;
+    }
+}
