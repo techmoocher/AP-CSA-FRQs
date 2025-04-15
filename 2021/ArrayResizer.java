82d293bf13795e407854133dbd33d38aef411cb3
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-15 22:07:57 +0700

    Create ArrayResizer.java

---
+++ 2021/ArrayResizer.java
@@ -0,0 +1,19 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 15, 2025
+*/
+
+/* Question 4 */
+public class ArrayResizer {
+    public static int numNonZeroRows(int[][] array2D) {
+        /* implementation not shown */
+    }
+    
+    public static boolean isNonZeroRow(int[][] array2D, int r) {
+        
+    }
+
+    public static int[][] resize(int[][] array2D) {
+        
+    }
+}
