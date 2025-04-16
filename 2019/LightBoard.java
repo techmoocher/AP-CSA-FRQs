27ed51938e97a5f9107098513d8fe5dfcefd3548
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-16 20:44:39 +0700

    Create LightBoard.java

---
+++ 2019/LightBoard.java
@@ -0,0 +1,18 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 16, 2025
+*/
+
+/* Question 4 */
+
+public class LightBoard {
+    private boolean[][] lights;
+    
+    public LightBoard(int numRows, int numCols) {
+        
+    }
+    
+    public boolean evaluateLight(int row, int col) {
+        
+    }
+}
