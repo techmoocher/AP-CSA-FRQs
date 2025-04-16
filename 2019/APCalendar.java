009b1107cb85c3698185ff905ea857542f86acd4
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-16 19:05:42 +0700

    Create APCalendar.java

---
+++ 2019/APCalendar.java
@@ -0,0 +1,19 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 16, 2025
+*/
+
+/* Question 1 */
+
+public class APCalendar {
+	private static boolean isLeapYear(int year) {
+		/* implementation not shown */
+	}
+	public static int firstDayOfYear(int year) {
+		/* implementation not shown */
+	}
+	private static int dayOfYear(int month, int day, int year) {
+		/* implementation not shown */
+	}
+	
+}
