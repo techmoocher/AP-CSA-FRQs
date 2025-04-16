c6c7b4f4694a67efe0550d2a2d4496ffc285e1e4
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-16 20:52:50 +0700

    Update LightBoard.java

--- 2019/LightBoard.java
+++ 2019/LightBoard.java
@@ -9,7 +9,19 @@ public class LightBoard {
     private boolean[][] lights;
     
     public LightBoard(int numRows, int numCols) {
+        lights = new boolean[numRows][numCols];
         
+        for (int i = 0; i < lights.length; i++) {
+            for (int j = 0; j < lights[0].length; j++) {
+                int chance = (int) (Math.random() * (100)) + 1;
+                if (chance <= 40) {
+                    lights[i][j] = true;
+                }
+                else {
+                    lights[i][j] = false;
+                }
+            }
+        }
     }
     
     public boolean evaluateLight(int row, int col) {
