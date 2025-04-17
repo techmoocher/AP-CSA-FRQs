f651077419fa5eaed25f522ee5dab528766f37c0
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-17 21:26:32 +0700

    Update WordPairList.java

--- 2018/WordPairList.java
+++ 2018/WordPairList.java
@@ -6,3 +6,25 @@
 /* Question 2 */
 
 /* Implementation for WordPair class is not shown */
+
+public class WordPairList {
+    private ArrayList<WordPair> allPairs;
+    
+    public WordPairList(String[] words) {
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                allPairs.add(new WordPair(words[i], words[j]));
+            }
+        }
+    }
+    
+    public int numMatches() {
+        int count = 0;
+        for (WordPair item : allPairs) {
+            if (item.getFirst().equals(item.getSecond())) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
