5660318c31d9756637987622f441a81eb7dab9fb
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-17 21:38:00 +0700

    Update CodeWordChecker.java

--- 2018/CodeWordChecker.java
+++ 2018/CodeWordChecker.java
@@ -5,4 +5,28 @@
 
 /* Question 3 */
 
-/* Implementation for StringChecker class is not shown */
+/* Implementation for StringChecker interface is not shown */
+/* isValid is a method of StringChecker interface */
+public class CodeWordChecker implements StringChecker {
+    private int minLength, maxLength;
+    private String special;
+    
+    public CodeWordChecker(int minimum, int maximum, String str) {
+        minLength = minimum;
+        maxLength = maximum;
+        special = str;
+    }
+    
+    public CodeWordChecker(String str) {
+        minLength = 6;
+        maxLength = 20;
+        special = str;
+    }
+    
+    public boolean isValid(String item) {
+        if (item.length() >= minLength && item.length() <= maxLength && item.indexOf(special) == -1) {
+            return true;
+        }
+        return false;
+    }
+}
