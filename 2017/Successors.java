3edfdcdf58ab80f9c4f0e4d3500f5c265b797acc
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-18 10:42:54 +0700

    Update Successors.java

--- 2017/Successors.java
+++ 2017/Successors.java
@@ -6,6 +6,25 @@
 /* Question 4 */
 
 /* Implementation for Positition class is not shown */
-public class Successor {
-
+public class Successors {
+    public static Position findPosition(int num, int[][] intArr) {
+        for (int i = 0; i < intArr.length; i++) {
+            for (int j = 0; j < intArr[0].length; j++) {
+                if (int[i][j] == num) {
+                    return new Position(i, j);
+                }
+            }
+        }
+        return null;
+    }
+    
+    public static Position[][] getSuccessorArray(int[][] int intArr) {
+        Position[][] result = new Position[intArr.length][intArr[0].length];
+        for (int row = 0; row < intArr.length; row++) {
+            for (int col = 0; col < intArr[0].length; col++) {
+                result[row][col] = findPosition(intArr[row][col] + 1, intArr);
+            }
+        }
+        return result;
+    }
 }
