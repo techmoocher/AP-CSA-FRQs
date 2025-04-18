5f17cdafcdd9d9de9f52a3d9538e3b44b10bedf3
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-18 08:05:27 +0700

    Update Digits.java

--- 2017/Digits.java
+++ 2017/Digits.java
@@ -1,18 +1,30 @@
 /*
 *    Created by: {techmoocher}
-*    Date: Arpil 17, 2025
+*    Date: Arpil 18, 2025
 */
 
 /* Question 1 */
 
 public class Digits {
-	private ArrayList<Integer> digitList;
-
-	public Digits(int num) {
-
-	}
-
-	public boolean isStrictlyIncreasing() {
-
-	}
-]
+    private ArrayList<Integer> digitList;
+    
+    public Digits(int num) {
+        digitList = new ArrayList<Integer>();
+        if (num == 0) {
+            digitList.add(0);
+        }
+        while (num > 0) {
+            digitList.add(0, new Integer(num % 10));
+            num /= 10;
+        }
+    }
+    
+    public boolean isStrictlyIncreasing() {
+        for (int i = 0; i < digitList.size() - 1; i++) {
+            if (digitList.get(i + 1) <= digitList.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
