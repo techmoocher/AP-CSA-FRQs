b17a510cd83e8fe53469ebf5c599d25119bc8739
Tech Moocher <92527665+techmoocher@users.noreply.github.com>
2025-04-18 07:41:13 +0700

    Create Digits.java

---
+++ 2017/Digits.java
@@ -0,0 +1,18 @@
+/*
+*    Created by: {techmoocher}
+*    Date: Arpil 17, 2025
+*/
+
+/* Question 1 */
+
+public class Digits {
+	private ArrayList<Integer> digitList;
+
+	public Digits(int num) {
+
+	}
+
+	public boolean isStrictlyIncreasing() {
+
+	}
+]
